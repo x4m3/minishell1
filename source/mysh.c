eb16485d78644ee761411932da136fdb77fd53a9
Philippe Loctaux <p@philippeloctaux.com>
2019-01-14 20:10:53 +0100

    added simple function that prints a colored prompt

--- source/mysh.c
+++ source/mysh.c
@@ -5,9 +5,18 @@
 ** vim-epitech made by x4m3
 */
 
+#include <unistd.h> /* for execve */
 #include "mysh.h"
+#include "config.h"
+#include "colors.h"
 
-int main(int ac, char **av)
+void mysh(void)
 {
+    putput("%s%s%s ", c_bold_green, prompt, c_reset);
+}
+
+int main(void)
+{
+    mysh();
     return EXIT_OKAY;
 }
