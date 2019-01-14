97bd5d975b1d964b14185c9e5a4c5b2eac99843f
Philippe Loctaux <p@philippeloctaux.com>
2019-01-14 20:23:17 +0100

    include/colors.h: removed not used colors

--- include/colors.h
+++ include/colors.h
@@ -9,19 +9,7 @@
 #define COLORS_H
 
 static char *c_reset = "\033[0m";
-
-static char *c_red = "\033[0;31m";
-static char *c_green = "\033[0;32m";
-static char *c_yellow = "\033[0;33m";
-static char *c_blue = "\033[0;34m";
-static char *c_magenta = "\033[0;35m";
-static char *c_cyan = "\033[0;36m";
-
 static char *c_bold_red = "\033[1;31m";
 static char *c_bold_green = "\033[1;32m";
-static char *c_bold_yellow = "\033[1;33m";
-static char *c_bold_blue = "\033[1;34m";
-static char *c_bold_magenta = "\033[1;35m";
-static char *c_bold_cyan = "\033[1;36m";
 
 #endif
