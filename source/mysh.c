3a97c30b7311aa5607c1058a76ea4d820271be6a
Philippe Loctaux <p@philippeloctaux.com>
2019-01-22 15:42:36 +0100

    i can run commands now

--- source/mysh.c
+++ source/mysh.c
@@ -5,32 +5,72 @@
 ** vim-epitech made by x4m3
 */
 
+#include <stdio.h> /* for perror */
 #include <stdlib.h> /* for free */
 #include <unistd.h> /* for execve */
+#include <sys/wait.h> /* for waitpid */
 #include "mysh.h"
 #include "config.h"
 #include "colors.h"
 
-void mysh(void)
+void check_return(int *status)
+{
+    (WIFEXITED(*status)) ? putput("OK\n") : putput("nope\n");
+}
+
+void exec(char **command)
+{
+    execve(command[0], command, NULL);
+}
+
+int fork_exec(char **command, int *status)
+{
+    pid_t child = fork();;
+    if (child == -1) {
+        perror("failed to fork");
+        return 1;
+    }
+    (child == 0) ? exec(command) : 0;
+    if (waitpid(child, status, 0) > 0);
+    return 0;
+}
+
+char *prompt_get_input(void)
+{
+    char *input_command;
+
+    putput("%s%s%s ", c_bold_green, prompt, c_reset);
+    input_command = get_next_line(0);
+    return input_command;
+}
+
+int mysh(void)
 {
     char *input_command;
     char **command;
+    int status;
 
     while (1) {
-    putput("%s%s%s ", c_bold_green, prompt, c_reset);
-    input_command = get_next_line(0);
-    putput("\n");
-    if (input_command == NULL)
-        break;
-    command = str_to_word_array(input_command, ' ');
-    // execve
-    free_char_arr(command);
+        input_command = prompt_get_input();
+        if (input_command == NULL) {
+            putput("exit\n");
+            break;
+        }
+        command = str_to_word_array(input_command, ' ');
+        if (fork_exec(command, &status) != 0) {
+            perror("error! aborting.\n");
+            return 1;
+        }
+        check_return(&status);
+        free_char_arr(command);
     }
     free(input_command);
+    return 0;
 }
 
 int main(void)
 {
-    mysh();
-    return EXIT_OKAY;
+    if (mysh() == 1)
+        return 84;
+    return 0;
 }
