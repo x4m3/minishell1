12170ffc4db4fccc3057f459bef95c258a93ea1b
Philippe Loctaux <p@philippeloctaux.com>
2019-01-22 15:16:56 +0100

    added everything in a loop

--- source/mysh.c
+++ source/mysh.c
@@ -16,15 +16,17 @@ void mysh(void)
     char *input_command;
     char **command;
 
+    while (1) {
     putput("%s%s%s ", c_bold_green, prompt, c_reset);
     input_command = get_next_line(0);
-    if (input_command == NULL) {
-        free(input_command);
-        return;
-    }
+    putput("\n");
+    if (input_command == NULL)
+        break;
     command = str_to_word_array(input_command, ' ');
-    free(input_command);
+    // execve
     free_char_arr(command);
+    }
+    free(input_command);
 }
 
 int main(void)
