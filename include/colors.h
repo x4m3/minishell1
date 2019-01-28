b25f86fe0647965eedcfe5473689f23fe93d04a8
Philippe Loctaux <p@philippeloctaux.com>
2019-01-28 10:17:34 +0100

    colors works-ish

--- include/colors.h
+++ include/colors.h
@@ -8,8 +8,13 @@
 #ifndef COLORS_H
 #define COLORS_H
 
-static char *c_reset = "\033[0m";
-static char *c_bold_red = "\033[1;31m";
-static char *c_bold_green = "\033[1;32m";
+typedef struct s_colors t_colors;
+
+struct s_colors {
+    char *current;
+    char *c_reset;
+    char *c_bold_red;
+    char *c_bold_green;
+};
 
 #endif
--- source/mysh.c
+++ source/mysh.c
@@ -12,9 +12,14 @@
 #include "mysh.h"
 #include "colors.h"
 
-void check_return(int *status)
+void check_return(int *status, t_colors *colors)
 {
-    (WIFEXITED(*status)) ? 0 : putput("segmentation fault (core dumped)\n");
+    if (WIFEXITED(*status)) {
+        putput("segmentation fault (core dumped)\n");
+        colors->current = colors->c_bold_red;
+    }
+    else
+        colors->current = colors->c_bold_green;
 }
 
 int fork_exec(char **command, int *status, char **env)
@@ -23,50 +28,76 @@ int fork_exec(char **command, int *status, char **env)
     pid_t child = fork();
 
     if (parent != getpid()) {
-        if (execve(command[0], command, env) == -1)
+        if (execve(command[0], command, env) == -1) {
             putput("command not found\n");
         exit(0);
+        }
     }
     waitpid(child, status, 0);
     return 0;
 }
 
-char *prompt_get_input(void)
+char *prompt_get_input(t_colors *colors)
 {
     char *input_command;
 
-    putput("%s%s%s ", c_bold_green, "$", c_reset);
+    putput("%s%s%s ", colors->current, "$", colors->c_reset);
     input_command = get_next_line(0);
     return input_command;
 }
 
-int mysh(char **env)
+int loop(t_colors *colors, char **env)
 {
     char *input_command;
     char **command;
     int status;
 
-    while (1) {
-        input_command = prompt_get_input();
-        if (input_command == NULL) {
-            putput("exit\n");
-            return 0;
-        }
-        command = str_to_word_array(input_command, ' ');
-        if (fork_exec(command, &status, env) != 0) {
-            perror("error! aborting.\n");
-            return 1;
-        }
-        check_return(&status);
-        free_char_arr(command);
-        free(input_command);
+    input_command = prompt_get_input(colors);
+    if (input_command == NULL) {
+        putput("exit\n");
+        return -1;
     }
+    command = str_to_word_array(input_command, ' ');
+    if (fork_exec(command, &status, env) != 0) {
+        perror("error! aborting.\n");
+        return 1;
+    }
+    check_return(&status, colors);
+    free_char_arr(command);
+    free(input_command);
+    return 0;
+}
+
+t_colors *init_t_colors(void)
+{
+    t_colors *colors = malloc(sizeof(t_colors));
+
+    if (!colors)
+        return NULL;
+    colors->c_reset = "\033[0m";
+    colors->c_bold_red = "\033[1;31m";
+    colors->c_bold_green = "\033[1;32m";
+    colors->current = colors->c_bold_green;
+    return colors;
+}
+
+int mysh(char **env)
+{
+    t_colors *colors = init_t_colors();
+
+    if (!colors)
+        return -1;
+    while (1) {
+        if (loop(colors, env) == -1)
+            return 0;
+    }
+    free(colors);
     return 0;
 }
 
 int main(int ac, char **env)
 {
-    if (ac != 1 || mysh(env) == 1)
+    if (ac != 1 || mysh(env) != 0)
         return 84;
     return 0;
 }
