c0eced27bba5cff1934638f4b6317e127690f452
Philippe Loctaux <p@philippeloctaux.com>
2019-02-02 12:50:08 +0100

    trying something

--- source/exec.c
+++ source/exec.c
@@ -28,7 +28,6 @@ int check_command(char **command, int *status)
 {
     if (access(command[0], F_OK | X_OK) == -1) {
         *status = -1;
-        putput_err("%s: ", command[0]);
         return 1;
     }
     return 0;
@@ -67,9 +66,17 @@ int exec(char **command, int *status, char **env)
     if (check_command(command, status) == 0)
         return fork_exec(command, status, env);
     path = move_path_pointer(str_to_word_array(get_path(env), ':'));
-    command[0] = full_path_command(path[0], command[0]);
+    for (int i = 0; path[i]; i++) {
+        command[0] = full_path_command(path[i], dup_command);
+        if (check_command(command, status) == 0)
+            return fork_exec(command, status, env);
+    }
+    command[0] = dup_command;
     free(dup_command);
     if (check_command(command, status) == 0)
         return fork_exec(command, status, env);
+    putput_err("%s: ", command[0]);
+    free_char_arr(path);
+    *status = -1;
     return 0;
 }
